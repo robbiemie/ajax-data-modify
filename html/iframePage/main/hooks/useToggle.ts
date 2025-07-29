85b590a4182ffdc28e50f15e18f5c56ef454b0b6
robbiemie <yang657850144@163.com>
2025-07-29 20:33:06 +0800

    feat: remove UNetwork panel

--- html/iframePage/main/hooks/useToggle.ts
+++ html/iframePage/main/hooks/useToggle.ts
@@ -3,11 +3,16 @@ import { useState } from 'react';
 export const useToggle = () => {
   const [ajaxToolsSwitchOn, setAjaxToolsSwitchOn] = useState(true); // 默认开启
   const [ajaxToolsExpandAll, setAjaxToolsExpandAll] = useState(false); // 默认开启
-  
+
+  const updateAjaxToolsSwitchOn = (value: boolean) => {
+    setAjaxToolsSwitchOn(value);
+  };
+
   return {
-    ajaxToolsSwitchOn, 
+    ajaxToolsSwitchOn,
     ajaxToolsExpandAll,
     setAjaxToolsSwitchOn,
-    setAjaxToolsExpandAll
+    setAjaxToolsExpandAll,
+    updateAjaxToolsSwitchOn,
   };
 };
